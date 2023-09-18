84bd5d6eb04656412a9709e087cc1e029e02de91
Ashley Cao <52900646+yuanc0000@users.noreply.github.com>
2023-09-18 01:29:50 +0000

    modify code to handle exception data

--- polars_data_analysis.py
+++ polars_data_analysis.py
@@ -1,7 +1,12 @@
 import polars as pl
 
-# Load the dataset using polars
-df_spotify_polars = pl.read_csv("spotify-2023.csv", encoding="ISO-8859-1")
+# Load the dataset using polars with ignore_errors and null_values parameters
+df_spotify_polars = pl.read_csv(
+    "spotify-2023.csv", 
+    encoding="ISO-8859-1", 
+    ignore_errors=True, 
+    null_values=["BPM110KeyAModeMajorDanceability53Valence75Energy69Acousticness7Instrumentalness0Liveness17Speechiness3"]
+)
 
 # Display the first few rows of the dataset
 print("=== Dataset Overview ===")
@@ -17,14 +22,8 @@ print("=== Summary Statistics ===")
 print(summary_statistics_polars)
 print("\n")
 
-# Handle non-numeric values in 'streams' column and convert it to integer type
-df_spotify_polars = df_spotify_polars.filter(df_spotify_polars['streams'].is_numeric())
-df_spotify_polars = df_spotify_polars.with_column(df_spotify_polars.col('streams').cast(pl.Int32))
-
 # Top 10 songs by streams
 top_10_songs_polars = df_spotify_polars.sort('streams', reverse=True).limit(10)
 print("=== Top 10 Songs by Streams ===")
 print(top_10_songs_polars.select(['track_name', 'artist(s)_name', 'streams']))
 print("\n")
-
-# For visualization, you can use other libraries like `matplotlib` and `seaborn` as in the original code.
